5be59fd0ccc3735c9bd9317a718ee58d0f77e2c3
peninah98 <peninangizwenayo@gmail.com>
2023-11-02 15:01:33 +0200

    feat: creating login compoonent

---
+++ src/componets/Login.jsx
@@ -0,0 +1,12 @@
+function Login() {
+  return (
+    <div className="bg-violet-900 w-1/2 h-1/2 space-y-7 px-4 py-8 text-center mx-auto my-20 shadow-lg">
+      <h1 className="text-white text-2xl">Login Here!</h1>
+      <button className="text-violet-900 bg-white rounded-md px-5 py-2 font-bold leading-9">
+        Login
+      </button>
+    </div>
+  );
+}
+
+export default Login;
