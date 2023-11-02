3c673aa6b5229dc7efa8e85e3337d7ea70355d82
peninah98 <peninangizwenayo@gmail.com>
2023-11-02 14:49:39 +0200

    Creating the development branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-center text-red-500 pt-20">Hello there!</h1>
+      <h1 className="text-center text-red-500 pt-20">Hello Geek!</h1>
     </>
   );
 }
