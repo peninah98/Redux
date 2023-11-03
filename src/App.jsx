c7840f00b09f94603ef7ff3eed4f49e7c5e07e0d
peninah98 <peninangizwenayo@gmail.com>
2023-11-03 12:11:52 +0200

    fix: fixing vertical margin of the div

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./componets/Login";
 
 function App() {
   return (
-    <div className="bg-violet-900 w-1/2 h-1/2 space-y-7 px-2 text-center py-8 mx-auto my-20 shadow-2xl">
+    <div className="bg-violet-900 w-1/2 h-1/2 space-y-7 px-2 text-center py-8 mx-auto my-4 shadow-2xl">
       <h1 className="text-4xl text-gray-400 ">React Redux</h1>
       <Profile/>
       <Login/>
